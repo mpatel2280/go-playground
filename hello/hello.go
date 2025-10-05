ce4e56b1e274b22074c6c59f497c329e27fe1ab2
Manish Patel <patelm.2280@gmail.com>
2025-10-05 14:48:58 +0530

    error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	greetings "module-example/greetings"
 )
 
 func main() {
-	message := greetings.Hello("World")
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
